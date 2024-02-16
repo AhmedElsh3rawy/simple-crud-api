c219b4a652c2aca05b8d262c6436f66e01ce8464
Ahmed <ahmedelsharawy117@gmail.com>
2024-02-16 23:49:09 +0200

    modified users routes

--- routes/users.js
+++ routes/users.js
@@ -5,41 +5,40 @@ import { v4 as uuidv4 } from "uuid";
 const router = express.Router();
 
 let users = [
-  {
-    firstName: "John",
-    lastName: "Wick",
-    age: 30,
-    id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-  },
-  {
-    firstName: "Dean",
-    lastName: "Winshester",
-    age: 25,
-    id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb0x",
-  },
+	{
+		firstName: "John",
+		lastName: "Wick",
+		age: 30,
+		id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+	},
+	{
+		firstName: "Dean",
+		lastName: "Winchester",
+		age: 25,
+		id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb0x",
+	},
 ];
 
 router.get("/", (req, res) => {
-  console.log(users);
-  res.send(users);
+	res.send(users);
 });
 
 router.get("/:userId", (req, res) => {
-  const { id } = req.params.userId;
-  const user = users.find((user) => user.id === id);
-  res.send(user);
+	const id = req.params.userId;
+	const user = users.find((user) => user.id === id);
+	res.send(user);
 });
 
 router.post("/", (req, res) => {
-  const user = req.body;
-  users.push({ ...user, id: uuidv4() });
-  res.send(`The user with the id: ${user.id} added to the database`);
+	const user = req.body;
+	users.push({ ...user, id: uuidv4() });
+	res.send(`The user with the id: ${user.id} added to the database`);
 });
 
 router.delete("/:userId", (req, res) => {
-  const { id } = req.params.userId;
-  users.filter((user) => user.id !== id);
-  res.send(`The user with the id: ${id} deleted from the database`);
+	const id = req.params.userId;
+	users = users.filter((user) => user.id !== id);
+	res.send(`The user with the id: ${id} deleted from the database`);
 });
 
 export default router;
