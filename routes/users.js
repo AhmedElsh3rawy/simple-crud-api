fba6845de945cdaa85022d98d68b1ecef48fbb85
Ahmed <ahmedelsharawy117@gmail.com>
2024-02-17 17:59:00 +0200

    added put & patch requests

--- routes/users.js
+++ routes/users.js
@@ -26,19 +26,42 @@ router.get("/", (req, res) => {
 router.get("/:userId", (req, res) => {
 	const id = req.params.userId;
 	const user = users.find((user) => user.id === id);
+
 	res.send(user);
 });
 
 router.post("/", (req, res) => {
 	const user = req.body;
 	users.push({ ...user, id: uuidv4() });
+
 	res.send(`The user with the id: ${user.id} added to the database`);
 });
 
 router.delete("/:userId", (req, res) => {
 	const id = req.params.userId;
 	users = users.filter((user) => user.id !== id);
+
 	res.send(`The user with the id: ${id} deleted from the database`);
+});
+
+router.patch("/:userId", (req, res) => {
+	const id = req.params.userId;
+	const { firstName, lastName, age } = req.body;
+	let user = users.find((user) => user.id === id);
+
+	if (firstName) user.firstName = firstName;
+	if (lastName) user.lastName = lastName;
+	if (age) user.age = age;
+
+	res.send(`The user with the id: ${id} modified`);
+});
+
+router.put("/:userId", (req, res) => {
+	const id = req.params.userId;
+	users = users.filter((user) => user.id !== id);
+	const user = req.body;
+	users.push({ ...user, id: uuidv4() });
+	res.send(`Updated`);
 });
 
 export default router;
